04f544bf5b6d74be476eeeeb215d56498c08e043
Nicolaspz <adao.nicolau@weeconnect.co.ao>
2024-11-12 15:34:46 +0100

    update migration

--- prisma/migrations/20241103212501_update_user_enumtipe_pagemnto/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `tipo_pagemento` on the `users` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Enum(EnumId(2))`.
-
-*/
--- AlterTable
-ALTER TABLE `users` MODIFY `tipo_pagemento` ENUM('SEMANAL', 'QUINZENAL', 'MENSAL') NOT NULL;
